51fec537e89de50ab15884fb2b9029428d22bbd0
Maharshi Shukla <shukla.maha201@gmail.com>
2024-05-24 20:54:32 +0530

    Core JS Assignment1

--- coreJS.js
+++ coreJS.js
@@ -1,5 +1,9 @@
+//NONE QUESTION IS FOR CHECKING JUST UPLOADED TO CHECK MY PROGRESS IN % SHOWN IN COURSE
+
+
+
 //Question 2
-let a = 10;
+/*let a = 10;
 let b =25;
 for(let c = a+1; c<=b;c++) console.log(a,b,c);
 
@@ -10,4 +14,14 @@ let num =11;
 (num > 0)?(console.log("Positive")):((num == 0)?(console.log("Zero")):(console.log("Negative")));
 
 
-//Question 4
+//Question 5
+const username ="admin";
+const password ="123";
+
+if (username === "admin" && password === "12345") {
+    console.log("Login Successful"); 
+} 
+else {
+    console.log("Invalid credentials");
+} 
+*/
