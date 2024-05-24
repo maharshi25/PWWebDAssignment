a62795db6f5e4a2ac70f9158f50947cce79e6494
Maharshi Shukla <shukla.maha201@gmail.com>
2024-05-24 20:52:23 +0530

    Core JS Assignment1

---
+++ coreJS.js
@@ -0,0 +1,13 @@
+//Question 2
+let a = 10;
+let b =25;
+for(let c = a+1; c<=b;c++) console.log(a,b,c);
+
+
+
+//Question 3
+let num =11;
+(num > 0)?(console.log("Positive")):((num == 0)?(console.log("Zero")):(console.log("Negative")));
+
+
+//Question 4
