ad6c2c1b88903f8ef0839e5b020ba04f6db54fc7
Maharshi Shukla <shukla.maha201@gmail.com>
2024-05-25 10:09:19 +0530

    Core JS Assignment2

--- coreJS2.js
+++ coreJS2.js
@@ -1,17 +1,14 @@
 //NONE QUESTION IS FOR CHECKING JUST UPLOADED TO CHECK MY PROGRESS IN % SHOWN IN COURSE
 
 
-/*
 //Question 1
 const sqr = (x) => (x*x) ;
-
 console.log(sqr(5));
 
 
 //Question 2
 const ages = [19,22,19,24,20,25,26,24,25,24];
 ages.sort((a,b)=> a-b);
-
 const minAge = ages[0];
 const maxAge = ages[ages.length -1];
 let median =0;
@@ -21,7 +18,6 @@ if(ages.length % 2 == 1){
     median = (ages[ages.length/2] + ages[ages.length/2 +1])/2
 }
 let avg;
-
 console.log(minAge, maxAge, median, avg);
 
 
@@ -35,12 +31,10 @@ contactMap.set("Agastya",{
     age : 16,
     email :"agastya@gmail.com"
 })
-
 let getContact = (name) => {
     return contactMap.get(name);
 }
 console.log(getContact("Maharshi"))
-*/
 
 
 //Question 4
@@ -57,4 +51,3 @@ let introduce = (objname)=>{
 }
 introduce(person1);
 introduce(person2);
-
